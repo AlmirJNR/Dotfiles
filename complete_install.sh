85f80c4a21ed8a380d9d319aafdec5a715d34ca0
Almir Jr <almirafjr.dev@gmail.com>
2021-12-12 17:31:44 -0300

    Add dotfiles (zshrc)

--- complete_install.sh
+++ complete_install.sh
@@ -11,3 +11,7 @@ source "${PWD}/install_scripts/install_packages.sh"
 # Install VSCode extensions ------------------------------
 echo "> Installing all the required VSCode extensions..."
 source "${PWD}/install_scripts/install_vscode_extensions.sh"
+
+# Install dotfiles ------------------------------
+echo "> Installing all the required dotfiles..."
+source "${PWD}/configs/install_zshrc.sh"
