a9b5b8084e9ed22443341a46c4ec652459646f6f
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2024-05-01 19:48:08 -0300

    feat!: update dotfiles installation script

--- configs/install_zshrc.sh
+++ configs/install_zshrc.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/bash
 
-mv .configs/dot_zshrc ~/.zshrc
-mv .configs/dot_p10k ~/.p10k.zsh
+ln -s .configs/.zshrc ~/.zshrc
+ln -s .configs/.p10k.zsh ~/.p10k.zsh
+ln -s .configs/.gitconfig ~/.gitconfig
