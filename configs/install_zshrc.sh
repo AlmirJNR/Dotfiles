289b80ed1ec6d470e275d7f827ee0b73f722f04b
Almir Alves de Freitas Junior <almirafjr.dev@gmail.com>
2024-05-07 13:40:19 -0300

    feat: add .profile and symbolic linking for .profile and .zprofile

--- configs/install_zshrc.sh
+++ configs/install_zshrc.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/bash
 
+ln -s .configs/.profile ~/.profile
+ln -s .configs/.profile ~/.zprofile
 ln -s .configs/.zshrc ~/.zshrc
 ln -s .configs/.p10k.zsh ~/.p10k.zsh
 ln -s .configs/.gitconfig ~/.gitconfig
